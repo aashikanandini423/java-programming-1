ecf5d9e7f0f1aaf3e67e84a92e740edaea38562a
aashikanandini423 <aashika.nandini423@gmail.com>
2022-10-24 00:50:21 -0700

    First assignment

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+         Scanner scanner = new Scanner(System.in);
+
+        // write your program here
+        System.out.println("Give a number:");
+        int value = Integer.valueOf(scanner.nextLine());
+        System.out.println("You gave the number "+value);
+         
+    }
+}

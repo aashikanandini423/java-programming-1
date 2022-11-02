a8a113d6070690be8ebd06b14c1b5cd95e74eaa4
aashikanandini423 <aashika.nandini423@gmail.com>
2022-11-02 00:29:14 -0700

    Part04-5-9

---
+++ Part04/part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
@@ -0,0 +1,15 @@
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(2);
+
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
+    }
+}
